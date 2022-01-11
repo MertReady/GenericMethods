115e7a3699fa862ef78d3c78e781b01f816c516a
Mert Baykar <mert.baykar@ready.com.tr>
2022-01-11 10:36:05 +0300

    quickfix for names

--- GenericMethods/SimpleMethods.cs
+++ GenericMethods/SimpleMethods.cs
@@ -7,7 +7,7 @@ namespace GenericMethods
 {
     public static class SimpleMethods
     {
-        public static IOrderedEnumerable<T> WhereOrderBy<T,Tkey>(this IEnumerable<T> myCollection, Func<T, bool> predicate,Func<T,Tkey> key) where T : class
+        public static IOrderedEnumerable<T> GetListWhereOrderBy<T,TKey>(this IEnumerable<T> myCollection, Func<T, bool> predicate,Func<T,TKey> key) where T : class
         {
             var result = myCollection.Where(predicate).OrderBy(key);
             return result;
