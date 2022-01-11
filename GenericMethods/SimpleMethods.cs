a73c2253be8fd5858195b02a61f0a954aba31ead
Mert Baykar <mert.baykar@ready.com.tr>
2022-01-11 10:04:52 +0300

    Add project files.

---
+++ GenericMethods/SimpleMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace GenericMethods
+{
+    public static class SimpleMethods
+    {
+        public static IOrderedEnumerable<T> WhereOrderBy<T,Tkey>(this IEnumerable<T> myCollection, Func<T, bool> predicate,Func<T,Tkey> key) where T : class
+        {
+            var result = myCollection.Where(predicate).OrderBy(key);
+            return result;
+        }
+    }
+}
