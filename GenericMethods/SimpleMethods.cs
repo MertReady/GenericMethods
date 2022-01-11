433786cfe5a374f476d88e8e9668dbf7ea94ce54
Mert Baykar <mert.baykar@ready.com.tr>
2022-01-11 16:45:32 +0300

    to be continued

--- GenericMethods/SimpleMethods.cs
+++ GenericMethods/SimpleMethods.cs
@@ -7,10 +7,29 @@ namespace GenericMethods
 {
     public static class SimpleMethods
     {
-        public static IOrderedEnumerable<TSource> GetEnumerableWhereOrderBy<TSource, TKey>(this IEnumerable<TSource> myCollection, Func<TSource, bool> predicate, Func<TSource, TKey> key) where TSource : class where TKey : struct
+        #region Linq
+
+        public static IOrderedEnumerable<TSource> GetFilteredListByOrder<TSource, TKey>(this IEnumerable<TSource> mainCollection, Func<TSource, bool> predicate, Func<TSource, TKey> key) where TSource : class where TKey : struct
         {
-            var result = myCollection.Where(predicate).OrderBy(key);
+            var result = mainCollection.Where(predicate).OrderBy(key);
             return result;
         }
+        public static IOrderedEnumerable<TSource> GetFilteredListByReverseOrder<TSource, TKey>(this IEnumerable<TSource> mainCollection, Func<TSource, bool> predicate, Func<TSource, TKey> key) where TSource : class where TKey : struct
+        {
+            var result = mainCollection.Where(predicate).OrderByDescending(key);
+            return result;
+        }
+
+        public static IOrderedQueryable<TSource> GetFilteredListByOrder<TSource, TKey>(this IQueryable<TSource> mainCollection, Expression<Func<TSource, bool>> predicate, Expression<Func<TSource, TKey>> key) where TSource : class where TKey : struct
+        {
+            var result = mainCollection.Where(predicate).OrderBy(key);
+            return result;
+        }
+        public static IOrderedQueryable<TSource> GetFilteredListByReverseOrder<TSource, TKey>(this IQueryable<TSource> mainCollection, Expression<Func<TSource, bool>> predicate, Expression<Func<TSource, TKey>> key) where TSource : class where TKey : struct
+        {
+            var result = mainCollection.Where(predicate).OrderByDescending(key);
+            return result;
+        }
+        #endregion
     }
 }
